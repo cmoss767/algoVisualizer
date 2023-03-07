c899b89a16ca1be82ef4c4a1a6cde77730c43467
Chris Moss <hcmoss70@gmail.com>
2023-03-06 23:07:09 -0500

    fixed routing error

--- server/main.py
+++ server/main.py
@@ -2,10 +2,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run(host='localhost', debug=True)
+
 
 @app.route("/")
 def hello():
     return "Hello there"
+
+if __name__ == '__main__':
+    app.run(host="localhost")
 
